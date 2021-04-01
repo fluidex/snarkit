a50f2da17aae824326b0a98bcd1c650292860675
张卓 <mycinbrin@gmail.com>
2021-04-01 16:19:44 +0800

    fix: fix command options

--- dist/cli.js
+++ dist/cli.js
@@ -6,12 +6,13 @@ const commander_1 = require("commander");
 async function main() {
     try {
         const program = new commander_1.Command();
+        program.version('0.0.2');
         program
             .command('compile <circuit_dir>')
             .description('compile a circom circuit dir')
             .option('-f, --force_recompile', 'ignore compiled files', false)
             .option('-v, --verbose', 'print verbose log', true)
-            .option('-b, --backend', 'native or wasm', 'wasm')
+            .option('-b, --backend <string>', 'native or wasm', 'wasm')
             .action(async (circuit_dir, options) => {
             await index_1.compileCircuitDir(circuit_dir, {
                 alwaysRecompile: options.force_recompile,
@@ -22,18 +23,18 @@ async function main() {
         program
             .command('check <circuit_dir>')
             .alias('test')
-            .option('-d, --data_dir', 'all input.json/output.json inside this dir will be tested', '')
+            .option('-d, --data_dir <string>', 'all input.json/output.json inside this dir will be tested', '')
             .option('-f, --force_recompile', 'ignore compiled files', false)
             .option('-v, --verbose', 'print verbose log', true)
-            .option('-b, --backend', 'native or wasm', 'wasm')
-            .option('-w, --witness_type', 'bin or text', 'text')
+            .option('-b, --backend <string>', 'native or wasm', 'wasm')
+            .option('-w, --witness_type <string>', 'bin or text', 'text')
             .description('test a circom circuit with given inputs/outputs')
             .action(async (circuit_dir, options) => {
             await index_1.testCircuitDir(circuit_dir, options.data_dir, {
                 alwaysRecompile: options.force_recompile,
                 verbose: options.verbose,
                 backend: options.backend,
-                witnessFileType: options.witness_type == "bin" ? "wtns" : "json"
+                witnessFileType: options.witness_type == 'bin' ? 'wtns' : 'json',
             });
         });
         await program.parseAsync(process.argv);
